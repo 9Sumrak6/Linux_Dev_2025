71eb1d1469987f77b3f16a228e15f5f82e87d88c
9Sumrak6 <step@Ascorbinka.ascorbinka.org>
2025-11-16 21:55:20 +0300

    Delete extra

--- 08_I18n/run.sh
+++ 08_I18n/run.sh
@@ -12,10 +12,6 @@ cmake ..
 echo "=== Сборка ==="
 make
 
-# echo "=== Проверка структуры ==="
-# echo "Содержимое build:"
-# find . -name "*.mo" -type f | xargs -I {} ls -la {} | head -20
-
 echo ""
 echo "=== Запуск программы ==="
 LANGUAGE=ru_RU.UTF-8 LANG=ru_RU.UTF-8 LC_ALL=ru_RU.UTF-8 ./bin/guess
